d4946c92286b0003ab22fde1e2c7e7ad94fc4217
Fernanda Lowe and Gigi Ditz <fernanda@>
2016-04-05 11:03:50 -0700

    create address constructor

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,17 @@
 function Contact(first, last) {
   this.firstName = first;
   this.lastName = last;
+  this.addresses = [];
+}
+function Address(street, city, state) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
 }
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
+
 
 // user interface logic
 $(document).ready(function() {
